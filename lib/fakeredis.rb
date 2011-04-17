0b87b7e9ed10ded6f3bfa258ec019207daa9d6ac
Guillermo Iguaran <guilleiguaran@gmail.com>
2011-04-16 20:17:04 -0500

    Adding FakeRedis::Redis::Client

--- lib/fakeredis.rb
+++ lib/fakeredis.rb
@@ -1,6 +1,30 @@
 
 module FakeRedis
   class Redis
+    class Client
+      attr_accessor :host, :port, :db, :path, :password, :logger, :reconnect
+      def initialize(options = {})
+        @path      = options[:path]
+        @host      = options[:host] || "127.0.0.1"
+        @port      = (options[:port] || 6379).to_i
+        @password  = options[:password]
+        @db        = (options[:db] || 0).to_i
+        @logger    = options[:logger]
+        @reconnect = true
+      end
+
+      def connect
+        self
+      end
+
+      def connected?
+        true
+      end
+
+      def method_missing(command, *args, &block)
+        true
+      end
+    end
 
     def self.connect(options = {})
       new(options)
@@ -9,6 +33,11 @@ module FakeRedis
     def initialize(options = {})
       @data = {}
       @expires = {}
+      @client = Client.new
+    end
+
+    def client
+      @client
     end
   end
 end

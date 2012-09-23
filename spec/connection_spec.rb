95b35b085ce775c7f22d131dfe13f7614f55ac78
Caius Durling <dev@caius.name>
2012-09-23 23:02:35 +0100

    Spec for instance caching
    It should only care about the host & port combination, as that's all that differentiates live redis instances.

--- spec/connection_spec.rb
+++ spec/connection_spec.rb
@@ -11,6 +11,28 @@ module FakeRedis
     it "should authenticate to the server" do
       @client.auth("pass").should == "OK"
     end
+
+    it "should re-use the same instance with the same host & port" do
+      @client1 = Redis.new(host: "localhost", port: 1234)
+      @client2 = Redis.new(host: "localhost", port: 1234)
+      @client3 = Redis.new(host: "localhost", port: 5678)
+
+      @client1.set("key1", "1")
+      @client2.get("key1").should == "1"
+      @client3.get("key1").should be_nil
+
+      @client2.set("key2", "2")
+      @client1.get("key2").should == "2"
+      @client3.get("key2").should be_nil
+
+      @client3.set("key3", "3")
+      @client1.get("key3").should be_nil
+      @client2.get("key3").should be_nil
+
+      @client1.dbsize.should == 2
+      @client2.dbsize.should == 2
+      @client3.dbsize.should == 1
+    end
   end
 
     it "should echo the given string" do

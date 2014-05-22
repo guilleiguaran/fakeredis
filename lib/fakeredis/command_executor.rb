dabb8598c89681121e4c6d88157f8eab05db33a7
Larry Lv <larrylv1990@gmail.com>
2014-05-22 19:26:34 +0800

    Don't alias any methods, just re-write `write` command.

--- lib/fakeredis/command_executor.rb
+++ lib/fakeredis/command_executor.rb
@@ -2,7 +2,11 @@ module FakeRedis
   module CommandExecutor
     def write(command)
       meffod = command.shift.to_s.downcase.to_sym
-      if respond_to?(meffod)
+
+      if in_multi && !(TRANSACTION_COMMANDS.include? meffod) # queue commands
+        queued_commands << [meffod, *command]
+        reply = 'QUEUED'
+      elsif respond_to?(meffod)
         reply = send(meffod, *command)
       else
         raise Redis::CommandError, "ERR unknown command '#{meffod}'"
--- lib/fakeredis/transaction_commands.rb
+++ lib/fakeredis/transaction_commands.rb
@@ -1,30 +1,7 @@
 module FakeRedis
-  module TransactionCommands
-    REDIS_COMMANDS = [
-      :append, :auth, :bgrewriteaof, :bgsave, :bitcount, :bitop, :blpop, :brpop,
-      :brpoplpush, :dbsize, :decr, :decrby, :del, :discard, :dump, :echo, :eval,
-      :evalsha, :exec, :exists, :expire, :expireat, :flushall, :flushdb, :get,
-      :getbit, :getrange, :getset, :hdel, :hexists, :hget, :hgetall, :hincrby,
-      :hincrbyfloat, :hkeys, :hlen, :hmget, :hmset, :hscan, :hset, :hsetnx,
-      :hvals, :incr, :incrby, :incrbyfloat, :info, :keys, :lastsave, :lindex,
-      :linsert, :llen, :lpop, :lpush, :lpushx, :lrange, :lrem, :lset, :ltrim,
-      :mget, :migrate, :monitor, :move, :mset, :msetnx, :multi, :object,
-      :persist, :pexpire, :pexpireat, :ping, :psetex, :psubscribe, :pttl,
-      :publish, :pubsub, :punsubscribe, :quit, :randomkey, :rename, :renamenx,
-      :restore, :rpop, :rpoplpush, :rpush, :rpushx, :sadd, :save, :scan,
-      :scard, :sdiff, :sdiffstore, :select, :set, :setbit, :setex, :setnx,
-      :setrange, :shutdown, :sinter, :sinterstore, :sismember, :slaveof,
-      :slowlog, :smembers, :smove, :sort, :spop, :srandmember, :srem, :sscan,
-      :strlen, :subscribe, :sunion, :sunionstore, :sync, :time, :ttl, :type,
-      :unsubscribe, :unwatch, :watch, :zadd, :zcard, :zcount, :zincrby,
-      :zinterstore, :zrange, :zrangebyscore, :zrank, :zrem, :zremrangebyrank,
-      :zremrangebyscore, :zrevrange, :zrevrangebyscore, :zrevrank, :zscan,
-      :zscore, :zunionstore
-    ]
-    TRANSACTION_REDIS_COMMANDS = [
-      :discard, :exec, :multi, :watch, :unwatch
-    ]
+  TRANSACTION_COMMANDS = [:discard, :exec, :multi, :watch, :unwatch]
 
+  module TransactionCommands
     def self.included(klass)
       klass.class_eval do
         def self.queued_commands
@@ -58,10 +35,9 @@ module FakeRedis
         raise Redis::CommandError, "ERR DISCARD without MULTI"
       end
 
-      revert_alias_redis_commands!
-
       self.in_multi = false
       self.queued_commands = []
+
       'OK'
     end
 
@@ -69,8 +45,6 @@ module FakeRedis
       unless in_multi
         raise Redis::CommandError, "ERR EXEC without MULTI"
       end
-
-      revert_alias_redis_commands!
 
       responses  = queued_commands.map do |cmd|
         begin
@@ -92,7 +66,6 @@ module FakeRedis
       end
 
       self.in_multi = true
-      alias_redis_commands!
 
       yield(self) if block_given?
 
@@ -105,42 +78,6 @@ module FakeRedis
 
     def unwatch
       "OK"
-    end
-
-    private
-
-    # Private: alias redis commands, and redefine them.
-    #
-    # Example:
-    #   Original `set` method will be aliased as `real_set` method,
-    #   and `set` method will just put command call to queued_commands.
-    #
-    def alias_redis_commands!
-      self.class.class_eval {
-        all_instance_methods = self.instance_methods.map(&:to_sym)
-        (REDIS_COMMANDS - TRANSACTION_REDIS_COMMANDS).each { |m|
-          next unless all_instance_methods.include? m
-
-          alias_method "real_#{m}".to_sym, m
-          define_method m do |*args|
-            queued_commands << [m, *args]
-            'QUEUED'
-          end
-        }
-      }
-    end
-
-    # Private: revert `alias_redis_commands!`
-    def revert_alias_redis_commands!
-      self.class.class_eval {
-        all_instance_methods = self.instance_methods.map(&:to_sym)
-        (REDIS_COMMANDS - TRANSACTION_REDIS_COMMANDS).each { |m|
-          next unless all_instance_methods.include? m
-
-          alias_method m, "real_#{m}".to_sym
-          undef_method "real_#{m}".to_sym
-        }
-      }
     end
   end
 end

6de0c006e51c355faf1e65f2c947e19e55e4cdb2
Tjalling van der Wal <tjalling.vanderwal@nedap.com>
2017-06-16 09:04:40 +0200

    CommandExecutor#write should not modify its argument

--- lib/fakeredis/command_executor.rb
+++ lib/fakeredis/command_executor.rb
@@ -1,13 +1,14 @@
 module FakeRedis
   module CommandExecutor
     def write(command)
-      meffod = command.shift.to_s.downcase.to_sym
+      meffod = command[0].to_s.downcase.to_sym
+      args = command[1..-1]
 
       if in_multi && !(TRANSACTION_COMMANDS.include? meffod) # queue commands
-        queued_commands << [meffod, *command]
+        queued_commands << [meffod, *args]
         reply = 'QUEUED'
       elsif respond_to?(meffod)
-        reply = send(meffod, *command)
+        reply = send(meffod, *args)
       else
         raise Redis::CommandError, "ERR unknown command '#{meffod}'"
       end

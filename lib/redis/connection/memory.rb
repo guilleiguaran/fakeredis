4d3f605f553fdcccf35e01821f7f82e53b4ac0a9
Tiago Teixeira <tiagocmtex@gmail.com>
2017-08-29 10:45:07 +0200

    Only support known commands
    Right now we only want to support the :setname and :getname sub commands for #client,
    other sub commands return error.

--- lib/redis/connection/memory.rb
+++ lib/redis/connection/memory.rb
@@ -89,7 +89,13 @@ class Redis
       def disconnect
       end
 
-      def client(command, options)
+      def client(command, options = {})
+        case command
+        when :setname then true
+        when :getname then nil
+        else
+          raise Redis::CommandError, "ERR unknown command '#{command}'"
+        end
       end
 
       def timeout=(usecs)

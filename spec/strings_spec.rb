86d6c91ad8ec5a68d089dbe731795c3a092181ac
Stephen Weil <stephen.weil@gmail.com>
2021-01-05 14:56:49 -0600

    Add failing test case for SETEX non-integer value

--- spec/strings_spec.rb
+++ spec/strings_spec.rb
@@ -242,6 +242,10 @@ module FakeRedis
       expect(@client.ttl("key1")).to eq(30)
     end
 
+    it "should raise an error if a non-integer is provided as the expiration" do
+      expect { @client.setex("key1", 1.5, "value1") }.to raise_error(Redis::CommandError)
+    end
+
     it "should set the value of a key, only if the key does not exist" do
       expect(@client.setnx("key1", "test value")).to eq(true)
       expect(@client.setnx("key1", "new value")).to eq(false)
